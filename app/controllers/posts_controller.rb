d34a448544f2ae30ea67ebee7b634cdf058b4bf1
Stephen Allard <stephencallard@gmail.com>
2020-08-24 21:00:05 -0500

    Add posts#edit and posts#update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
