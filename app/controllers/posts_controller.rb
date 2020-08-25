78688039b47d75faec63f402f3366c895833a298
Stephen Allard <stephencallard@gmail.com>
2020-08-24 21:13:59 -0500

    Add posts#delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_url
+  end
+
   private
 
   def post_params
